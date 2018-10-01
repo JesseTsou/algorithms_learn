1ec948048dcb32df6c1231db3c5ac96cf94014e5
JesseTsou <693939576@qq.com>
2018-10-01 11:06:53 +0800

    【sort】添加堆排序1

--- sort/main.cpp
+++ sort/main.cpp
@@ -5,7 +5,7 @@
 #include "sortFun.h"
 
 int main() {
-    int n = 200000;
+    int n = 100000;
     int *arr = sortTestHelper::generaterandomArray(n, 0, n);
     int *arr2 = sortTestHelper::copyIntArrary(arr,n);
     int *arr3 = sortTestHelper::copyIntArrary(arr,n);
@@ -13,16 +13,19 @@ int main() {
     int *arr5 = sortTestHelper::copyIntArrary(arr,n);
     int *arr6 = sortTestHelper::copyIntArrary(arr,n);
     int *arr7 = sortTestHelper::copyIntArrary(arr,n);
+    int *arr8 = sortTestHelper::copyIntArrary(arr,n);
     //selectionSort(arr,n);
     //sortTestHelper::printArray(arr, n);
 
-    //sortTestHelper::testSort("selection sort:", selectionSort,arr,n);
-    //sortTestHelper::testSort("insertion sort:", insertionSort,arr2,n);
-    //sortTestHelper::testSort("bubble    sort:", bubbleSort,arr3,n);
+    sortTestHelper::testSort("selection sort:", selectionSort,arr,n);
+    sortTestHelper::testSort("insertion sort:", insertionSort,arr2,n);
+    sortTestHelper::testSort("bubble    sort:", bubbleSort,arr3,n);
     sortTestHelper::testSort("merge     sort:", mergeSort,arr4,n);
     sortTestHelper::testSort("quick     sort:", quickSort,arr5,n);
     sortTestHelper::testSort("quick     sort2:", quickSort2,arr6,n);
     sortTestHelper::testSort("quick     sort3:", quickSort3way,arr7,n);
+    sortTestHelper::testSort("heap      sort:", heapsort,arr8,n);
+
 /*
  * 不同类型测试
     float b[5] = {4.1,4.2,0.0,3,0.1};
@@ -37,6 +40,8 @@ int main() {
     selectionSort(d,5);
     sortTestHelper::printArray(d, 5);
 */
+
     delete[] arr;
+
     return 0;
 }
